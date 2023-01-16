d655ecdf0e442f8d6466602f99e185968904a632
DahamWeon <onedaham@naver.com>
2023-01-16 11:20:44 +0900

    [추가] dice animation, setInterval, toggle click

--- client/main.js
+++ client/main.js
@@ -1,1 +1,29 @@
-console.log(window);
+import { diceAnimation, getNode } from "./lib/index.js";
+
+// [주사위 굴리기]
+// 1. dice 애니메이션 불러오기
+// 2. bindeEvent 유틸함수 만들기
+// 3. handlerRollingDice 함수 만들고 토글로 애니메이션 제어하기
+// 4. 변수 보호를 위한 클로저 + IIFE 사용하기
+
+const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)');
+
+// IIFE
+const handlerRollingDice = (() => {
+
+  let isRolling = false;
+  let stopAnimation;
+  
+  return() => {
+    if(!isRolling){
+      stopAnimation = setInterval(diceAnimation,100);
+    } else{
+      clearInterval(stopAnimation);
+    }
+    isRolling = !isRolling;
+  }
+
+  })()
+
+
+rollingDiceButton.addEventListener('click',handlerRollingDice);
